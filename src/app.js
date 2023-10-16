15a75944add3639f19b28b3d586793c794e2ed4e
Robert Allison <rober.r.allison4@gmail.com>
2023-10-16 15:45:30 -0500

    help

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 
+const API_URL = 'http://localhost:4280/rest/DownEquipmentDropdown';
+
 async function list() {
     const endpoint = '/data-api/rest/DownEquipmentDropdown';
     const response = await fetch(endpoint);
