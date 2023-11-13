b3e0f95b28f555db2eeaad765bf6824b994c7215
Robert Allison <rober.r.allison4@gmail.com>
2023-11-13 15:12:20 -0600

    oops

--- src/editPage.js
+++ src/editPage.js
@@ -493,7 +493,7 @@ function collectFormData() {
     return {
         // Retrieve values from the form inputs and dropdowns
         entry_timestamp: new Date().toISOString(), // Current timestamp in ISO format
-        user_name: document.getElementById("namesDropdown").selected.value, 
+        user_name: document.getElementById("namesDropdown").selected, 
         plant: document.getElementById("plantDropdown").value, 
         selection_date: document.getElementById("date").value, 
         // selection_name: "",
